f5e7e600cffb5f157e7b3014b081e302c9cc5cd2
Guilherme Estrela <guilherme.estrela@clickbus.com>
2019-06-24 20:07:55 -0300

    Add Book class, change to BookShelf and get data from BooksApi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import {Link, Route} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Search from './Search';
-import BookList from './BookList';
+import BookShelf from './BookShelf';
 import './App.css'
 
-class BooksApp extends React.Component {
+export default class BooksApp extends React.Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  getBooks() {
+    BooksAPI.getAll().then((books) => {console.log(books); this.setState({books})});
+  }
+
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-            <BookList />
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+
+                <BookShelf
+                    books={this.state.books.filter((book) => book.shelf === 'currentlyReading')}
+                    shelf={'Currently Reading'} />
+                <BookShelf
+                    books={this.state.books.filter((book) => book.shelf === 'wantToRead')}
+                    shelf={'Want to Read'}/>
+                <BookShelf
+                    books={this.state.books.filter((book) => book.shelf === 'read')}
+                    shelf={'Read'}/>
+              </div>
+              <div className="open-search">
+                <Link to="/search" className="add-book">Add a book</Link>
+              </div>
+            </div>
         )}/>
         <Route path="/search" render={() => (
             <Search />
@@ -19,5 +50,3 @@ class BooksApp extends React.Component {
     )
   }
 }
-
-export default BooksApp
---
+++ src/BookShelf.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Book from './Book';
+
+export default class BookShelf extends React.Component {
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.shelf}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.books.map((book) => <Book book={book} key={book.id}/>)}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
